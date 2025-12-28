c61c6870739422568cc1110cfce8ef54c695ec91
Muhammad Ashir Malik <ashir112291@gmail.com>
2025-12-28 15:53:45 +0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      "https://router.huggingface.co/models/gpt2",
+      "https://api-inference.huggingface.co/models/gpt2",
       {
         method: "POST",
         headers: {
