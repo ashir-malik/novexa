877588884ea7be6cc364558009c707a8fc2b23a9
Muhammad Ashir Malik <ashir112291@gmail.com>
2025-12-28 16:01:46 +0500

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     const response = await fetch("https://api-inference.huggingface.co/models/gpt2", {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${process.env.HF_TOKEN}`, // your token
+        "Authorization": `Bearer ${process.env.HF_TOKE}`, // your token
         "Content-Type": "application/json"
       },
       body: JSON.stringify({ inputs: prompt })
